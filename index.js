d5ecea120d4bfbeec468e258725d3a9f7cfb9e36
joyasturay <joyasturay1@gmail.com>
2024-12-20 22:14:08 +0530

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const path = require('path');
 const app = express();
 
-// Set storage options for multer (saving files locally)
+//storage options for multer (saving files locally)
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads/'); 
@@ -13,19 +13,17 @@ const storage = multer.diskStorage({
     },
 });
 
-// Initialize multer with the storage configuration
+
 const upload = multer({ storage: storage });
 
-// Serve static files from the "uploads" folder
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Route to handle video upload
 app.post('/upload', upload.single('video'), (req, res) => {
     const videoUrl = `/uploads/${req.file.filename}`; 
     res.json({ videoUrl });
 });
 
-// Start the server
 app.listen(3000, () => {
     console.log('Server started on http://localhost:3000');
 });
